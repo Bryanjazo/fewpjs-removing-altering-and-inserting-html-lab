a571ae4ff51ee55f2e10d40aa58fff20554f4dbf
Bryan Jazo <bryanjazo88@gmail.com>
2021-04-29 15:53:44 -0700

    commit tree 806b505f28c7cd5ada93ba34f4133cf5cd64abd5

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code h
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory";
+newHeader.className = 'victory'
+newHeader.innerHTML = "Bryan is the champion!";
